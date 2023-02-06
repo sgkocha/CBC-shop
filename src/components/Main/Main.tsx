63026bf0d0b0b1f4bbb26f2f186ff1947b921d85
sgkochar <sgkochar@gmail.com>
2023-02-06 14:59:10 +0200

    Add shop-info-caption

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -6,6 +6,12 @@ const Main = () => {
     <main>
         <div className="shop-about d-f">
             <CatalogLinks/>
+            <div className="shop-info">
+                <div className="shop-info-caption">
+                    <div className="shop-info-title">Computer store <span>CBC in Newark</span> </div>
+                    <div className="shop-info-desc">Supply of computer equipment, service center and services for IT integration companies</div>
+                </div>
+            </div>
         </div>
     </main>
   )
